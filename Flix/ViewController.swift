5cd3616463e87f786317110a7db2e0c29c39cc27
user145766 <user145766@74-80-245-57.ic2net.net>
2018-09-14 19:30:33 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by user145766 on 9/14/18.
+//  Copyright © 2018 Kenia Rioja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
