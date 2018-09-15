7393f65c2326bc83ae20ffd64c65eec74e9446ed
user145766 <user145766@74-80-245-57.ic2net.net>
2018-09-15 14:31:46 -0700

    set up network request, parsed through JSON data, made dictionary

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,61 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by user145766 on 9/15/18.
+//  Copyright © 2018 Kenia Rioja. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //! ("banger") causes it to force unwrap. if it was nil your app would crash
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        //cachecPolicy is this way so that it pulls data each time from the network for testing purposes
+        //last field is in seconds
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        //this coordinates all our network requests
+        //delegateQueue: when our network request returns it'll jump back in our main thread
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        //task to actually go out and get the data
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //this will run when the network request returns
+            //netwrok requests are asynchronous
+            //our main queue handles ui updates
+            //if they weren't netwrok requests would clog up the pipe
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //cast this data in order to get it from JSON form
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+                //print(dataDictionary)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
